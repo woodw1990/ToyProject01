724af8119eed48d9420ecbd4797629bd804b3c52
woodw1990 <icarus04111@gmail.com>
2022-07-04 16:56:43 +0900

    dd

--- toy_View/toy_View/MainWindow.xaml.cs
+++ toy_View/toy_View/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls;
 
 namespace toy_View
 {
@@ -23,6 +23,11 @@ namespace toy_View
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void dtp_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
